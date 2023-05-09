21690f385fca8d080928015f4a5468221943c7c3
Vivek Nair <vivekmanju53@gmail.com>
2023-05-10 01:33:47 +0530

    added timeline feature

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true});
