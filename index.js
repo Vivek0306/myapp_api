c888b16f30312df65e0b2727c523fdb637875c52
Vivek Nair <vivekmanju53@gmail.com>
2023-05-17 21:21:49 +0530

    added multar for image storage on server

--- index.js
+++ index.js
@@ -7,15 +7,39 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const multer  = require("multer")
+const path = require("path")
+
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
+app.use("/images", express.static(path.join(__dirname, "public/images")))
+
+
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+    destination:(req, file, cb) => {
+        cb(null, "public/images");
+    },
+    filename: (req, file, cb) =>{
+        cb(null, req.body.name)
+    }
+})
+const upload = multer({storage});
+
+app.post("/api/upload", upload.single("file"), (req, res)=>{
+    try{
+        return res.status(200).json("File uploaded!")
+    }catch(err){
+        console.log(err)
+    }
+})
 
 app.get("/", (req, res)=>{
     res.send("Welcome to homepage")
